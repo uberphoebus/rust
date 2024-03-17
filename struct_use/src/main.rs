2832ac6b53e00bb0310e83a8a6d1b4ba5031bae8
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-17 10:15:58 +0000

    structs

--- struct_use/src/main.rs
+++ struct_use/src/main.rs
@@ -5,6 +5,9 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -12,6 +15,30 @@ fn main() {
         email: String::from("uberphoebus@gmail.com"),
         sign_in_count: 1,
     };
-    
     user1.email = String::from("uberphoebus@naver.com");
+    println!("{}, {}", user1.username, user1.email);
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let mut _user3 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
