a938ec67ebf19668f6ee14713c85b28eea16ded0
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-24 13:09:59 +0000

    string

--- string/src/main.rs
+++ string/src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::new();
+    let data = "initial contents";
+    let s1 = data.to_string();
+    let s2 = "initial contents".to_string();
+    let s3 = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 = {s2}");
+
+let s1 = String::from("Hello, ");
+let s2 = String::from("world!");
+let s3 = s1 + &s2;
+    println!("s3 = {s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s4 = format!("{s1}-{s2}-{s3}");
+    println!("s4 = {s4}");
+
+    // let s1 = String::from("hello");
+    // let h = s1[0];
+
+let hello = "Здравствуйте";
+let s = &hello[0..4];
+
+for c in "Зд".chars() {
+    println!("{c}");
+}
+
+for b in "Зд".bytes() {
+    println!("{b}");
+}
+
 }
