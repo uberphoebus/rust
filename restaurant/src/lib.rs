a0677f2b61e1ff0483bf5393d9a2dae536c6bc33
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-24 11:38:57 +0000

    fin module

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
     mod serving {
@@ -9,3 +9,69 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // crate::front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+mod back_of_house2 {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant2() {
+    // 호밀 (Rye) 토스트를 곁들인 여름철 조식 주문하기
+    let mut meal = back_of_house2::Breakfast::summer("Rye");
+    // 먹고 싶은 빵 바꾸기
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // 다음 라인의 주석을 해제하면 컴파일되지 않습니다; 식사와 함께
+    // 제공되는 계절 과일은 조회나 수정이 허용되지 않습니다
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+mod back_of_house3 {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant3() {
+    let order1 = back_of_house3::Appetizer::Soup;
+    let order2 = back_of_house3::Appetizer::Salad;
+}
+
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+// --생략--
+use std::{cmp::Ordering, io};
+// --생략--
