263a9e8ad7bb776f754e863fcd41077af20710f3
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-24 13:40:54 +0000

    minigrep impl

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;  // for file read
 fn main() {
     let args: Vec<String> = env::args().collect(); // string to vec
     // dbg!(args);
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
@@ -17,9 +17,10 @@ struct Config {
     query: String,
     file_path: String,
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config {query, file_path}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config {query, file_path}
+    }
 }
