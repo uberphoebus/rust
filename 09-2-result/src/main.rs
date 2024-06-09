13f84e66922502f31a2922f109e7f6b30ebcf726
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-06-09 02:29:39 +0000

    add ch09

---
+++ 09-2-result/src/main.rs
@@ -0,0 +1,58 @@
+// use std::fmt::Result;
+use std::fs::{self, File};
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+    let _greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", error)
+            }
+        },
+    };
+
+    let _greeting_file = File::open("hello.txt").unwrap();
+    let _greeting_file = File::open("hello.txt").expect("hello.txt 파일을 열 수 없습니다.");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_short() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
